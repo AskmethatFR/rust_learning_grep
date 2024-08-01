5ab871a9a6529f97c9325f078d80baddfc942877
Alexandre Teixeira <ateixeira@askmethat.fr>
2024-08-01 13:57:36 +0200

    fix: remove comments in main

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,19 @@ use crate::grep::file_read_gateway::FileReaderGateway;
 
 mod grep;
 
-fn test(str: String) {
+fn say_hello(str: String) {
     println!("{}", str);
 }
 
 fn main() {
     let x = "Hello, world!".to_string();
-    test(x);
+    say_hello(x);
     
-    println!("{}", x);
-
-    // let file_reader_gateway = FileReaderGateway::new();
-    // 
-    // let file = file_reader_gateway.read_file("src/main.rs");
-    // 
-    // for line in file {
-    //     println!("{}", line);
-    // }
+    let file_reader_gateway = FileReaderGateway::new();
     
+    let file = file_reader_gateway.read_file("src/main.rs");
+    
+    for line in file {
+        println!("{}", line);
+    }
 }
